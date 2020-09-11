9ab550ceeb72c4bc32bb3db2cff6788e1b85d261
srujanjill <srujanjilla15@gmail.com>
2020-09-11 14:36:28 +0530

    Completed !

--- functions/index.js
+++ functions/index.js
@@ -3,76 +3,87 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 // auth trigger (new user signup)
-exports.newUserSignUp = functions.auth.user().onCreate((user) => {
-  // for background triggers you must return a value/promise
-  return admin.firestore().collection("users").doc(user.uid).set({
-    email: user.email,
-    upvotedOn: [],
+exports.newUserSignUp = functions
+  .region("asia-south1")
+  .auth.user()
+  .onCreate((user) => {
+    // for background triggers you must return a value/promise
+    return admin.firestore().collection("users").doc(user.uid).set({
+      email: user.email,
+      upvotedOn: [],
+    });
   });
-});
 
 // auth trigger (user deleted)
-exports.userDeleted = functions.auth.user().onDelete((user) => {
-  const doc = admin.firestore().collection("users").doc(user.uid);
-  return doc.delete();
-});
+exports.userDeleted = functions
+  .region("asia-south1")
+  .auth.user()
+  .onDelete((user) => {
+    const doc = admin.firestore().collection("users").doc(user.uid);
+    return doc.delete();
+  });
 
 // http callable function (adding a request)
-exports.addRequest = functions.https.onCall((data, context) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      "unauthenticated",
-      "only authenticated users can add requests"
-    );
-  }
-  if (data.text.length > 30) {
-    throw new functions.https.HttpsError(
-      "invalid-argument",
-      "request must be no more than 30 characters long"
-    );
-  }
-  return admin.firestore().collection("requests").add({
-    text: data.text,
-    upvotes: 0,
+exports.addRequest = functions
+  .region("asia-south1")
+  .https.onCall((data, context) => {
+    if (!context.auth) {
+      throw new functions.region('asia-south1').https.HttpsError(
+        "unauthenticated",
+        "only authenticated users can add requests"
+      );
+    }
+    if (data.text.length > 30) {
+      throw new functions.region('asia-south1').https.HttpsError(
+        "invalid-argument",
+        "request must be no more than 30 characters long"
+      );
+    }
+    return admin.firestore().collection("requests").add({
+      text: data.text,
+      upvotes: 0,
+    });
   });
-});
 
 // // upvote callable function
-exports.upvote = functions.https.onCall(async (data, context) => {
-  // check auth state
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      "unauthenticated",
-      "only authenticated users can vote up requests"
-    );
-  }
-  // get refs for user doc & request doc
-  const user = admin.firestore().collection("users").doc(context.auth.uid);
-  const request = admin.firestore().collection("requests").doc(data.id);
+exports.upvote = functions
+  .region("asia-south1")
+  .https.onCall(async (data, context) => {
+    // check auth state
+    if (!context.auth) {
+      throw new functions.region('asia-south1').https.HttpsError(
+        "unauthenticated",
+        "only authenticated users can vote up requests"
+      );
+    }
+    // get refs for user doc & request doc
+    const user = admin.firestore().collection("users").doc(context.auth.uid);
+    const request = admin.firestore().collection("requests").doc(data.id);
 
-  const doc = await user.get();
-  // check thew user hasn't already upvoted
-  if (doc.data().upvotedOn.includes(data.id)) {
-    throw new functions.https.HttpsError(
-      "failed-precondition",
-      "You can only vote something up once"
-    );
-  }
+    const doc = await user.get();
+    // check thew user hasn't already upvoted
+    if (doc.data().upvotedOn.includes(data.id)) {
+      throw new functions.region('asia-south1').https.HttpsError(
+        "failed-precondition",
+        "You can only vote something up once"
+      );
+    }
 
-  // update the array in user document
-  await user.update({
-    upvotedOn: [...doc.data().upvotedOn, data.id],
+    // update the array in user document
+    await user.update({
+      upvotedOn: [...doc.data().upvotedOn, data.id],
+    });
+
+    // update the votes on the request
+    return request.update({
+      upvotes: admin.firestore.FieldValue.increment(1),
+    });
   });
-
-  // update the votes on the request
-  return request.update({
-    upvotes: admin.firestore.FieldValue.increment(1),
-  });
-});
 
 // firestore trigger for tracking activity
-exports.logActivities = functions.firestore
-  .document("/{collection}/{id}")
+exports.logActivities = functions
+  .region("asia-south1")
+  .firestore.document("/{collection}/{id}")
   .onCreate((snap, context) => {
     console.log(snap.data());
 
